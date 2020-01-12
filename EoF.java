3335d3cbdd26b86c76e1a0a2bd89f45fe9580c0b
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-01-12 18:08:43 +0530

    Create EoF.java

---
+++ EoF.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Solution {
+
+    public static void main(String[] args) 
+    {
+        int i=1;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext())
+        {
+            System.out.println(i + " " + sc.nextLine());
+            i++;
+        }
+    }
+}
+
