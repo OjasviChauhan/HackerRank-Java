1286bd034f4792bd5b523c651c64374e956e0388
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-01-17 17:19:00 +0530

    Create Exception handling(try - catch).java

---
+++ Exception handling(try - catch).java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try
+        {
+            int x,y,r;
+            Scanner sc = new Scanner(System.in);
+            x=sc.nextInt();
+            y=sc.nextInt();
+            r=x/y;
+           if(y==0)
+                 throw  new ArithmeticException("/ by zero");
+             else
+                 System.out.println(x/y);
+        }
+        catch(InputMismatchException e)
+            {
+                System.out.println("java.util.InputMismatchException");
+            }
+        catch(ArithmeticException e)
+        {
+            System.out.println(e);
+        }
+    }
+}
